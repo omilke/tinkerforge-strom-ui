10f384baa22119580b327f5a531f65e61e01a30b
omilke <ollimi85@gmail.com>
2015-10-13 22:15:14 +0200

    Strom-Graph Layout verfeinert

--- src/main/java/de/d4k/tinkerforge/stromui/main/stromgraph/StromgraphPresenter.java
+++ src/main/java/de/d4k/tinkerforge/stromui/main/stromgraph/StromgraphPresenter.java
@@ -3,18 +3,33 @@ package de.d4k.tinkerforge.stromui.main.stromgraph;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
 
 /**
  * @author Oliver Milke
  */
 public class StromgraphPresenter implements Initializable {
+	
+	@FXML
+	LineChart<String, Number> stromChart;
 
 
 	@Override
 	public void initialize(final URL url, final ResourceBundle rb) {
 
+		prepareChart(stromChart);
 
+	}
+
+
+	private void prepareChart(LineChart<String, Number> chart) {
+
+		
+		chart.getXAxis().setTickLabelRotation(45);
+
+		chart.getYAxis().setLabel("[mA]");
 	}
 
 
